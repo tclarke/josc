75480d0aee01e54498b5a92cda4d755e02abd742
Trevor R.H. Clarke <tclarke@ball.com>
2012-10-19 23:15:58 -0400

    Added bundle support. Created initial osc proxy and a test client.

--- osc_proxy.js
+++ osc_proxy.js
@@ -1,11 +1,18 @@
 var util = require('util');
 var osc = require('./lib/osc');
+
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port:8080});
 wss.on('connection', function(ws) {
+   var oss = new osc.UDPServer(4444);
+   oss._dispatch = function(pkt) {
+      ws.send(pkt.sendToStream());
+   };
    ws.on('message', function(data) {
       var p = new osc.OSCPacket();
       p.recvFromStream(data);
       console.log(util.inspect(p, false, null));
       ws.close();
    });
+   oss.start()
 });
+
--- osc_test_client.js
+++ osc_test_client.js
@@ -4,16 +4,17 @@ var m = new osc.OSCMessage('/test/1');
 m.push(42);
 m.push(14);
 var m2 = new osc.OSCMessage('/test/2');
-var p = new osc.OSCPacket();
-p.push(m);
-p.push(m2);
+
+var bndl = new osc.OSCBundle();
+bndl.push(m);
+bndl.push(m2);
+var p = new osc.OSCPacket(bndl);
 
 var WebSocket = require('ws'), ws = new WebSocket('ws://localhost:8080');
 ws.on('open', function() {
    console.log(m);
    console.log(m2);
-   p.dispatchStream = function(d) { ws.send(d); };
-   p.send();
+   ws.send(p.sendToStream());
 });
 
 ws.on('message', function(message) {
